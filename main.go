5017668b2a239949dddbc30bf1a99827a3248d50
GYJ <guyongjian5@gmail.com>
2020-12-24 18:37:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ package main
 
 int SetTitle()
 {
-	SetConsoleTitle("GYJ Remote Shutdown");
+	SetConsoleTitle("GYJ Remote Control");
 	return 0;
 }
 */
@@ -54,15 +54,33 @@ func restartcc(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte("success"))
 }
 
+func lock(w http.ResponseWriter, r *http.Request) {
+	_ = r.ParseForm()
+	fmt.Println("状态: 锁屏执行成功！")
+	command := exec.Command("cmd", "/C", "rundll32.exe user32.dll LockWorkStation")
+	err := command.Run()
+	if err != nil {
+		//fmt.Println(err.Error())
+	}
+
+	_, _ = w.Write([]byte("success"))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html><title>GYJ</title><h1><center><br><br><br><br><br><br>GYJ Remote Control Services<br><br><br><br><br><br></h1></center><hr><h3><center>&copy; 2021 GYJ Corporation</center></h3></html>\n")
+}
+
 func main() {
 	C.SetTitle()
-	fmt.Println("GYJ 远程关机")
+	fmt.Println("GYJ 远程控制服务")
 	fmt.Println("\n日志:")
 	fmt.Println("状态：启动成功")
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
 	mux.HandleFunc("/shutdown-jcegbCFFGRWYwYq1Y4bH", shutdown)
 	mux.HandleFunc("/restart-jcegbCFFGRWYwYq1Y4bH", restartcc)
 	mux.HandleFunc("/stop-jcegbCFFGRWYwYq1Y4bH", termination)
+	mux.HandleFunc("/lock-jcegbCFFGRWYwYq1Y4bH", lock)
 	err := http.ListenAndServe(":25250", mux)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
